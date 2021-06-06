90681b9e0c4fc12c11870798560716bc81aa58dc
minhnngbh18582 <75022914+minhnngbh18582@users.noreply.github.com>
2021-06-06 19:54:03 +0700

    Initial commit

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,12 @@
+class NewsController {
+    // [get] /news
+    index(req, res) {
+        res.render('news');
+    }
+
+    show(req, res) {
+        res.send('NEW DETAIL!!!');
+    }
+}
+
+module.exports = new NewsController();
